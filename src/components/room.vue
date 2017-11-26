e666ea2da12ae27bd84e036862d8b15b314417b8
kanyaruk <ahmkanyaruk@hotmail.com>
2017-11-27 02:01:19 +0700

    change view room

--- src/components/room.vue
+++ src/components/room.vue
@@ -1,88 +1,71 @@
 <template>
-  <div class="hello">
-    <h1 class="title has-text-light">Battle Ship Game </h1>
-    <div class="columns">
-      <div class="column">
-        <img :src="user.fb && user.fb.photoURL" class="photo-url" alt="">
-        <h2 class="subtitle has-text-light">{{user.name}}</h2>
-        <h2 class="subtitle has-text-primary">{{roomId}}</h2>
-        <div class="control" >
-          <input type="button " value="Ready" class="button is-link">
+  <div>
+      <div class="challenge">CHALLENGE </div>
+      
+      <div class="display">
+        <div class="player1">
+          <img :src="player1Img" alt="" width="200" height="200">
+        </div>
+
+        <div class="vs">vs</div>
+
+        <div class="player2">
+          <img :src="player2Img" alt="" width="200" height="200">
         </div>
       </div>
-      <div class="column">
-        <img :src="user.fb && user.fb.photoURL" class="photo-url" alt="">
-        <h2 class="subtitle has-text-light">{{user.name}}</h2>
-        <h2 class="subtitle has-text-primary">{{roomId}}</h2>
-        <div class="control">
-          <input type="button " value="Ready" class="button is-link">
-        </div>
+
+      <div class="ready">
+        <button class="button is-primary is-large" @click="Playgame()">READY</button>
       </div>
-    </div>
-    <br>
+
   </div>
 </template>
 
 <script>
-import { mapGetters, mapActions } from 'vuex'
 export default {
-  name: 'Placeship',
   data () {
     return {
-      boardOnplay: '0011',
-      roomId: ''
+      player1Img: 'https://scontent.fbkk7-3.fna.fbcdn.net/v/t1.0-9/18425062_1404765559591464_6092264720532385167_n.jpg?oh=b6f50bff1886675e4fbc44154adc19c4&oe=5AA46F0F',
+      player2Img: 'https://scontent.fbkk7-3.fna.fbcdn.net/v/t1.0-9/23167635_10208738615657641_1577952619552424141_n.jpg?oh=fac91354d9b7c517f2a2aad0a15cc52e&oe=5A96E5CA'
     }
   },
-  methods: {
-    ...mapActions([
-      'getroom',
-      'init'
-    ])
-  },
-  computed: {
-    ...mapGetters([
-      'rooms',
-      'user'
-    ]),
-    undaterooms () {
-      this.getroom()
+  methods:{
+    Playgame() {
+      this.$router.push('/Playgame')
     }
-  },
-  created () {
-    this.roomId = this.$route.params.roomId
-    this.init()
   }
 }
 </script>
 
-<!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-  td{
-    border:2px solid rgba(0, 204, 255, 0.1);
-    width:50px;
-    height:50px;
-  }
-  table{
-    width:auto;
-    height:auto;
-  }
-  .img {
-    width:40px;
-    height:40px;
-  }
-  .ship{
-    background-color: rgb(194, 70, 174);
-    margin: 10px;
-  }
-  .boom {
-    background-color: rgba(203, 74, 10, 0.74);
-  }
-  .empty {
-    background-color: rgba(70, 142, 194,0.3);
-  }
-  .photo-url {
-  width: 48px;
-  height: 48px;
-  border-radius: 24px;
-}
+.challenge{
+  margin-top: 20px;
+  margin-bottom: 50px;
+  font-size: 20pt;
+   font-weight: 1000;
+} 
+.player1{
+   display: inline-block;
+  
+ }
+ .vs{
+  display: inline-block;
+  margin-top: 80px;
+  margin-left: 50px;
+  margin-right: 50px;
+  font-size: 20pt;
+  font-weight: 1000;
+  vertical-align: top;
+ }
+ .player2{
+   display: inline-block;
+ }
+ .ready{
+  margin-top: 90px; 
+  margin-left: 50px;
+  margin-right: 50px;
+  font-size: 20pt;
+  font-weight: 1000;
+ }
+
 </style>
