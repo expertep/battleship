b89e5522ef8d3f103b823a40597ffbe2d862f7ec
kittitahmine <32786185+kittitahmine@users.noreply.github.com>
2017-12-14 21:06:22 +0700

    css

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -13,10 +13,22 @@
     </router-link>
     </a>
     
-    <div>
-      <h1 class= "title is-1"> Battleship </h1>
-      <h2 class ="title is-4"> Login Facebook </h2>
-      <button class="button" @click="login()" v-if="!user">login</button>
+    <div style = "font-family: 'Alfa Slab One', cursive;">
+      <h1 class= "title is-0"> Welcome </h1>
+      <img src="../assets/battleship.png" class="img">
+      <br ><br ><br ><br ><br >
+      <h3 class ="title is-4"> Login Facebook </h3>
+
+
+      <div class="container">
+        <img src="../assets/login1.png" class="img">
+        <div class="overlay">
+          <div class = "text" @click="login()" v-if="!user" >
+              <img src="../assets/login2.png" class="img">
+          </div>
+        </div>
+      </div>
+
     </div>
   </div>
 </template>
@@ -55,5 +67,50 @@ export default {
   width: 48px;
   height: 48px;
   border-radius: 24px;
+}
+h1 {
+    color: white;
+    font-size: 80px;
+    text-shadow: 5px 5px 5px #aaa;
+}
+h2 {
+    color: white;
+    text-shadow: 3px 3px 3px #aaa;
+}
+h3 {
+    color: white;
+    text-shadow: 2px 2px 2px #aaa;
+}
+
+.container {
+  position: relative;
+  width: 50%;
+}
+
+.image {
+  display: block;
+  width: 100%;
+  height: auto;
+}
+
+.overlay {
+  top: 0;
+  bottom: 0;
+  left: 0;
+  right: 0;
+  opacity: 0;
+  transition: .5s ease;
+  background-color: #008CBA;
+}
+
+.container:hover .overlay {
+  opacity: 1;
+}
+
+.text {
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
 }
 </style>
